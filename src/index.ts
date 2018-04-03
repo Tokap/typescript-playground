1057e722976ecd92ed0ac93d60aa7f209a6ef0c2
Patrick Allen <AllenPatrick86@gmail.com>
2018-04-03 12:21:22 -0700

    Add additional TS examples to working project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+// ------------------------------------------------
+// Base Example of Making Interfaces (Structs):
+// ------------------------------------------------
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-let user = "Jane User";
+function greeter(person: Person) {
+    return "Hello, " + person.firstName;
+}
 
-greeter(user)
+let firstUser = "Jane User";
+
+let person = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+
+greeter(person);
+// ------------------------------------------------
+// Example Using ES6 Classes:
+// ------------------------------------------------
+class Student {
+    fullName: string;
+    constructor(
+        public firstName: string,
+        public middleInitial: string,
+        public lastName: string
+    ) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeterTwo(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+greeterTwo(user);
