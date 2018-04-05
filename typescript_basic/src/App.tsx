00923ce0e0f69f0ca53ea7f6b8682455abd9a1c0
Patrick Allen <AllenPatrick86@gmail.com>
2018-04-04 18:56:21 -0700

    Remove erroneous files and simplify example

--- typescript_basic/src/App.tsx
+++ typescript_basic/src/App.tsx
@@ -7,7 +7,12 @@ ReactDOM.render(
   <div>
     <h1>Hello, Welcome to the first page</h1>
     <FirstComponent />
-    <UserComponent name="Logrocket" age={105} address="get me if you can" dob={new Date()} />
+    <UserComponent
+      name="Logrocket"
+      age={105}
+      address="283 Las Vegas Blvd"
+      dob={new Date()}
+    />
   </div>,
   document.getElementById("root")
 );
