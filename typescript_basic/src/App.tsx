0181ee3c107acacbd47c9d9c80b8998b56979810
Patrick Allen <AllenPatrick86@gmail.com>
2018-04-04 17:20:30 -0700

    Add package to typescript_basic and begin building out configs

---
+++ typescript_basic/src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Let\'s Make a Cool Calendar</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
