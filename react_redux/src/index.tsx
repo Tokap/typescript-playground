54cc95d5bf63c1610677acbeb33d77a17779b721
Patrick Allen <AllenPatrick86@gmail.com>
2018-04-05 18:32:45 -0700

    Update ordering and notes

--- react_redux/src/index.tsx
+++ react_redux/src/index.tsx
@@ -5,16 +5,15 @@ import registerServiceWorker from "./registerServiceWorker";
 
 // Import Redux Provider Functionality for Store Manipulation
 import { Provider } from "react-redux";
-// Import the Hello wrapped container rather than component
-import Hello from "./containers/Hello";
-
-import "./index.css";
 // Import what we need from Redux to produce a local Store
 import { createStore } from "redux";
 // Import the inferface for our Stored State
 import { StoreState } from "./types/index";
 // Import reducer functionality
 import { enthusiasm } from "./reducers/index";
+// Import the Hello wrapped container rather than component
+import Hello from "./containers/Hello";
+import "./index.css";
 
 // Instantiate Local Store with Type definition
 const store = createStore<StoreState>(enthusiasm, {
--- react_routing/src/index.tsx
+++ react_routing/src/index.tsx
@@ -4,18 +4,13 @@ import * as ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
 // --- Redux Imports & General Functionality
-// Import Redux Provider Functionality for Store Manipulation
 import { Provider } from "react-redux";
-// Import the Hello wrapped container rather than component
-import Hello from "./containers/Hello";
-
-import "./index.css";
-// Import what we need from Redux to produce a local Store
 import { createStore } from "redux";
-// Import the inferface for our Stored State
 import { StoreState } from "./types/index";
-// Import reducer functionality
 import { enthusiasm } from "./reducers/index";
+
+import Hello from "./containers/Hello";
+import "./index.css";
 
 // --- React Router Imports
 import { BrowserRouter, Route, NavLink } from "react-router-dom";
@@ -46,7 +41,7 @@ ReactDOM.render(
       <Route path="/" component={RouterHello} exact={true} />
     </div>
   </BrowserRouter>,
-  document.getElementById("root") as HTMLElement // Declare Definite Return
+  document.getElementById("root") as HTMLElement
 );
 
 registerServiceWorker();
