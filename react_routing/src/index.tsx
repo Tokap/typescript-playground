5277a851e2de1286a4b72e5bb5613e399faf3465
Patrick Allen <AllenPatrick86@gmail.com>
2018-04-05 18:55:35 -0700

    Add notes, expand router example, extract reusable components

--- react_routing/src/index.tsx
+++ react_routing/src/index.tsx
@@ -9,11 +9,14 @@ import { createStore } from "redux";
 import { StoreState } from "./types/index";
 import { enthusiasm } from "./reducers/index";
 
-import Hello from "./containers/Hello";
+import Hello from "./containers/Hello"; // Wrapped Component -> Redux Container
+import Goodbye from "./components/extended/Goodbye"; // Straight Up Component
+
+import NavBar from "./components/extended/NavBar";
 import "./index.css";
 
 // --- React Router Imports
-import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 // Instantiate Local Store with Type definition
 const store = createStore<StoreState>(enthusiasm, {
@@ -27,18 +30,15 @@ const RouterHello = () => (
   </Provider>
 );
 
+// const RouterGoodbye = () => <Goodbye />;
+
 ReactDOM.render(
   <BrowserRouter>
     <div>
-      <ul>
-        <li>
-          <NavLink to="/" activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-      </ul>
+      <NavBar />
 
       <Route path="/" component={RouterHello} exact={true} />
+      <Route path="/goodbye" component={Goodbye} exact={true} />
     </div>
   </BrowserRouter>,
   document.getElementById("root") as HTMLElement
